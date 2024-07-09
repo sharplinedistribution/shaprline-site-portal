bcbb011b9d45e4ecf6013e3aed0b42a953374620
Muhammad Saad <63445891+saadjavaid67@users.noreply.github.com>
2024-07-10 01:42:19 +0500

    .

--- app/Http/Controllers/BulkReleaseController.php
+++ app/Http/Controllers/BulkReleaseController.php
@@ -20,7 +20,7 @@ class BulkReleaseController extends Controller
 
     public function show()
     {
-        $id = Session::put('release_id');
+        $id = Session::get('release_id');
         $user = User::findOrFail(auth()->user()->id); // Retrieve the authenticated user
         $release_data = Release::findOrFail($id); // Retrieve the release by its ID
         return view('user-portal.pages.revamp.releases.artist-show', compact('user', 'release_data'));
